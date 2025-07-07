4d37c993465d1c9735544f065768d9c64f02e3a1
Surendra12S <dasarisurendra0604@gmail.com>
2025-07-07 21:46:33 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing youser order fro frontend
 const placeOrder = async (req,res)=>{
 
-    const frontend_url = "http://localhost:5174";
+    const frontend_url = "https://dashboard.render.com/static/srv-d1luuep5pdvs73cgc8pg";
 
     try {
         const newOrder = new orderModel({
